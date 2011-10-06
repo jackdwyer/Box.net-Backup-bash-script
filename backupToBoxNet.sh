14bf45d0b06e54842830775193b5ea89c32140ee
Jack Dwyer <jackjack.dwyer@gmail.com>
2011-10-06 13:17:10 +0100

    Adding SQL Dump backups, need to create the mysqldump and rsync commands

--- backupToBoxNet.sh
+++ backupToBoxNet.sh
@@ -24,7 +24,7 @@ rs="/home/jack/Desktop/rSyncLog"
 source=("/home/jack/Desktop/src")
 destination=("/home/jack/Desktop/testDir")
 
-#Check both source and destination array are matching length
+#Validate both source and destination array are matching length
 if [ ${#source[@]} != ${#destination[@]} ]; then
 	#TODO EMAIL error the backup can not be ran.
 	echo "SOURCE AND DESTINATION ARRAYS DO NOT MATCH, backup can not be ran"
@@ -34,10 +34,19 @@ fi
 #mysql setup values
 password=a
 user=root
-sqlDump="/home/jack/Desktop/mysqlDump.sql"
+sqlSource=("/home/jack/Desktop/mysqlDump.sql")
+sqlDestination=("/home/jack/Desktop/dbDump")
 
-#total length for the looping
+#Validate sqlSource and sqlDestination are matching length
+if [ ${#sqlSource[@]} != ${#sqlDestination[@]} ]; then
+	#TODO append if an error to the error email
+	echo "SQL BACKUP ARRAYS DO NOT MATCH, backup can not run"
+	exit 1
+fi
+
+#Length of each array (sql, directories)
 backupLen=${#source[@]}
+mysqlLen=${#sqlSource[@]}
 
 errors=0
 
@@ -61,6 +70,19 @@ do
 	rSyncArray[$i]="rsync -a --log-file=${rs} ${source[$i]}  ${destination[$i]}"
 done
 
+if [ $mysqlLen != 0 ]; then
+	#First Generate SQL Dump file
+	for (( i=0; i<=$(( $mysqlLen - 1)); i++ ))
+	do
+		#Build array for mysql dump files and location on box.net to store
+	done
+
+	#append the new rsync commands to directory rsnyc array
+	for (( i=0; i<=$(( $mysqlLen -1)); i++ ))
+	do
+		#append the commands
+	done
+fi
 
 #--------------------
 #Actually do the backups
@@ -74,7 +96,7 @@ do
 done
 
 
-#Shoot off email to me, if the shit fucks up\
+#Shoot off email to me, if the shit fucks up
 if [ $errors -gt 0 ]; then
 	msmtp -t < ${rs}
 fi
